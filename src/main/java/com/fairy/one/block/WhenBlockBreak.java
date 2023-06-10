6c57b74d42060137e18a3d15358a6bb73eb33e30
ax3dg <ax3dgaming@gmail.com>
2023-06-10 08:54:03 +0200

    Space drop from endstone with space order enchant

--- src/main/java/com/fairy/one/block/WhenBlockBreak.java
+++ src/main/java/com/fairy/one/block/WhenBlockBreak.java
@@ -10,15 +10,18 @@ import net.minecraftforge.event.world.BlockEvent;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.item.enchantment.Enchantments;
 import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.util.Mth;
 import net.minecraft.core.BlockPos;
 
 import javax.annotation.Nullable;
+
+import java.util.Random;
 
 @Mod.EventBusSubscriber
 public class WhenBlockBreak {
@@ -35,11 +38,18 @@ public class WhenBlockBreak {
         if (entity == null)
             return;
         if ((world.getBlockState(new BlockPos(x, y, z))).getBlock() == Blocks.END_STONE
-                && EnchantmentHelper.getItemEnchantmentLevel(ModEnchantments.SPACE_ORDER.get(), (entity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY)) != 0) {
+                && EnchantmentHelper.getItemEnchantmentLevel(ModEnchantments.SPACE_ORDER.get(), (entity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY)) != 0 && Mth.nextInt(new Random(), 1, 10) == 5) {
             if (world instanceof Level _level && !_level.isClientSide()) {
                 ItemEntity entityToSpawn = new ItemEntity(_level, x, y, z, new ItemStack(ModItems.SPACE.get()));
                 entityToSpawn.setPickUpDelay(10);
                 _level.addFreshEntity(entityToSpawn);
+            }
+            while (entity instanceof Player _playerHasItem ? _playerHasItem.getInventory().contains(new ItemStack(Blocks.END_STONE)) : false) {
+                if (entity instanceof Player _player) {
+                    ItemStack _stktoremove = new ItemStack(Blocks.END_STONE);
+                    _player.getInventory().clearOrCountMatchingItems(p -> _stktoremove.getItem() == p.getItem(), 1, _player.inventoryMenu.getCraftSlots());
+                }
+                break;
             }
         }
     }
