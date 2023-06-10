db6102896c36a99559ca1b5a0378f329162542f4
Fairy <54899275+Fairy-Jeux@users.noreply.github.com>
2023-06-10 09:54:11 +0200

    Add SPAce Armor and Random Chance to drop SPAce with the enchantment SPAce Order.

--- src/main/java/com/fairy/one/item/ModItems.java
+++ src/main/java/com/fairy/one/item/ModItems.java
@@ -1,6 +1,8 @@
 package com.fairy.one.item;
 
 import com.fairy.one.One;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.item.ArmorItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.SwordItem;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -22,7 +24,18 @@ public class ModItems {
             () -> new SwordItem(ModTiers.SPACE, 5, 4f,
                     new Item.Properties().tab(ModCreativeModeTab.ONE_TAB)));
 
-
+    public static final RegistryObject<Item> SPACE_HELMET = ITEMS.register("space_helmet",
+            () -> new ArmorItem(ModArmorMaterials.SPACE, EquipmentSlot.HEAD,
+                    new Item.Properties().tab(ModCreativeModeTab.ONE_TAB)));
+    public static final RegistryObject<Item> SPACE_CHESTPLATE = ITEMS.register("space_chestplate",
+            () -> new ArmorItem(ModArmorMaterials.SPACE, EquipmentSlot.CHEST,
+                    new Item.Properties().tab(ModCreativeModeTab.ONE_TAB)));
+    public static final RegistryObject<Item> SPACE_LEGGING = ITEMS.register("space_leggings",
+            () -> new ArmorItem(ModArmorMaterials.SPACE, EquipmentSlot.LEGS,
+                    new Item.Properties().tab(ModCreativeModeTab.ONE_TAB)));
+    public static final RegistryObject<Item> SPACE_BOOTS = ITEMS.register("space_boots",
+            () -> new ArmorItem(ModArmorMaterials.SPACE, EquipmentSlot.FEET,
+                    new Item.Properties().tab(ModCreativeModeTab.ONE_TAB)));
 
     public static void register (IEventBus eventBus) {
         ITEMS.register(eventBus);
